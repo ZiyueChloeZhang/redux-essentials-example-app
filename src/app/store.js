af65bda5587cf9c64dfdf9a3fecfa68eba31faa4
Chloe Zhang <chloezhang1030@gmail.com>
2022-08-18 19:25:17 -0700

    creating posts slice, added to store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../features/posts/postsSlice';
+
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    // tells Redux that we want our top-level state object to have a field named posts inside
+    posts: postsReducer
+  }
 })
