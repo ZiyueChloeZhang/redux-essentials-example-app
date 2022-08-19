c6452d808c387fef4678f9d53b4281ae5497bfe6
Chloe Zhang <chloezhang1030@gmail.com>
2022-08-18 23:39:09 -0700

    author, date, reaction buttons

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import postsReducer from '../features/posts/postsSlice';
+import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export default configureStore({
   reducer: {
     // tells Redux that we want our top-level state object to have a field named posts inside
-    posts: postsReducer
-  }
+    posts: postsReducer,
+    users: usersReducer,
+  },
 })
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,24 +1,68 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' }
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+  },
 ]
 
 // responsible for handling all updates to the posts data
 const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers:{
-        // a reducer funtion
-        // createSlice() generate an action creator function with the same name 'postAdded'
-        // export the action to dispatch in components
-        postAdded(state, action) {
-            // this is ok because inside createSlice it use Immer library to make immutable updates
-            state.push(action.payload);
+  name: 'posts', // string action you see in redux devtool
+  initialState,
+  reducers: {
+    // {reducer, prepare}
+    // reducer is the reducer
+    // prepare is a callback function to prepare the payload (e.g. generating id)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            author: userId,
+          },
         }
-    }
+      },
+    },
+    // a reducer funtion
+    // createSlice() generate an action creator function with the same name 'postUpdated'
+    // export the action to dispatch in components
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
+  },
 })
 
-export const { postAdded }  = postsSlice.actions;
-export default postsSlice.reducer;
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+export default postsSlice.reducer
