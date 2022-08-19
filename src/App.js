ceda553854dd1a586bcedca4cb464bd005e901fd
Chloe Zhang <chloezhang1030@gmail.com>
2022-08-19 09:36:58 -0700

    added users page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import { AddPostForm } from './features/posts/AddPostForm'
 import { PostsList } from './features/posts/PostsList'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+
 function App() {
   return (
     <Router>
@@ -29,6 +32,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPosts/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
