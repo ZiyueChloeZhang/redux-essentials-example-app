3578e1946b3ae5299b5f0d99f523b1a270b55285
Chloe Zhang <chloezhang1030@gmail.com>
2022-08-18 19:44:35 -0700

    added postAdded action

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -5,10 +5,20 @@ const initialState = [
     { id: '2', title: 'Second Post', content: 'More text' }
 ]
 
+// responsible for handling all updates to the posts data
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers:{}
+    reducers:{
+        // a reducer funtion
+        // createSlice() generate an action creator function with the same name 'postAdded'
+        // export the action to dispatch in components
+        postAdded(state, action) {
+            // this is ok because inside createSlice it use Immer library to make immutable updates
+            state.push(action.payload);
+        }
+    }
 })
 
+export const { postAdded }  = postsSlice.actions;
 export default postsSlice.reducer;
